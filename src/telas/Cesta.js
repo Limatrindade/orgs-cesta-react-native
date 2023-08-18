a1fd22d5f10a418e2f0a3be84c2c61ff04bc0a57
Matheus Lima <teulima20@hotmail.com>
2023-08-18 19:33:28 -0300

    feat: configuring the basket component

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,7 +1,31 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, SafeAreaView } from "react-native";
+
+import topoImage from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
     return (
-        <Text>Cesta</Text>
+        <SafeAreaView>
+          <Image source={topoImage} style={styles.topo} />
+          <Text style={styles.title}>Detalhe da cesta</Text>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    title: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+});
