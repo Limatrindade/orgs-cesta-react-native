6890f517ef0788eb397ea2db232dff309792ef70
Matheus Lima <teulima20@hotmail.com>
2023-08-18 19:58:09 -0300

    feat: adding styling to the component

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,7 @@
-import { Image, StyleSheet, Dimensions, Text, SafeAreaView } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, SafeAreaView, View } from "react-native";
 
 import topoImage from '../../assets/topo.png';
+import logoFarm from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,6 +10,16 @@ export default function Cesta() {
         <SafeAreaView>
           <Image source={topoImage} style={styles.topo} />
           <Text style={styles.title}>Detalhe da cesta</Text>
+
+          <View style={styles.cesta}>
+            <Text style={styles.name}>Cesta de Verduras</Text>
+            <View style={styles.farm}>
+              <Image source={logoFarm} style={styles.imageFarm}/>
+              <Text style={styles.nameFarm}>Matheus Lima Farm</Text>
+            </View>
+            <Text style={styles.description}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+            <Text style={styles.price}>R$ 40,00</Text>
+          </View>
         </SafeAreaView>
     );
 }
@@ -27,5 +38,41 @@ const styles = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         padding: 16
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    name: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: "bold"
+    },
+    farm: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 12
+    },
+    imageFarm: {
+        width: 32,
+        height: 32
+    },
+    nameFarm: {
+        fontSize: 16,
+        lineHeight: 16,
+        marginLeft: 12
+    },
+    description: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    price: {
+        color: "#2a9f85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 });
